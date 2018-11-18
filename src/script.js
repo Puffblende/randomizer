89262d65e7f8599f7321da3a06123b14d3532559
Dennis Kiefer <dennis.kiefer.business@googlemail.com>
2018-11-18 11:14:34 +0100

    messed something up- and restored it then

--- src/script.js
+++ src/script.js
@@ -1,35 +1,3 @@
-/*let list = document.getElementById("list");
-let count = 0;
-let listArray = [];
-
-function add() {
-
-  let linebreak = document.createElement("br");
-  let node = document.createElement("LI");
-  let textnode = document.createTextNode(input.value);
-  node.appendChild(textnode);
-
-  document.getElementById("list").appendChild(node);
-  document.getElementById("list").appendChild(linebreak);
-
-  listArray.push(input.value);
-  count++;
-
-  if ( count <= 1 ) {
-    document.getElementById("button2").style.display = "none";
-  }
-  else {
-    document.getElementById("button2").style.display = "inline";
-  }
-}
-
-function generate(){
-  for(i = 0; i<=count; i++){
-    let random = Math.random()
-  }
-
-
-}*/
 $(function () {
 
   // Collect dom nodes
